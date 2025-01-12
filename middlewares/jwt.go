8d29f2a366272524d88f08d8c9d9ee896f7e33c3
MichaelSitanggang <michaelsitanggang37@gmail.com>
2025-01-12 22:35:26 +0700

    generate jwt done

--- middlewares/jwt.go
+++ middlewares/jwt.go
@@ -1,1 +1,38 @@
 package middlewares
+
+import (
+	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
+)
+
+// 1. header
+// 2. payload
+// 3. singnature
+
+var JwtKey = []byte(os.Getenv("JWT_SECRET_KEY"))
+
+type Claims struct {
+	AccountID int    `json:"accountID"`
+	Role      string `json:"role"`
+	jwt.RegisteredClaims
+}
+
+func GenerateJwt(accountID int, role string) (string, error) {
+	ExpiredTime := time.Now().Add(24 * time.Hour)
+	claims := Claims{
+		AccountID: accountID,
+		Role:      role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(ExpiredTime),
+			Issuer:    "BookStore",
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	tokenString, err := token.SignedString(JwtKey)
+	if err != nil {
+		return "", nil
+	}
+	return tokenString, nil
+}
